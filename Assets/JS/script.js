3759df8667e911bea2ed60bbabbe0f51410945bf
Elo-22 <aubert6064@gmail.com>
2021-07-27 16:11:25 +0200

    ex2part1java

---
+++ Assets/JS/script.js
@@ -0,0 +1,8 @@
+var lastname = " doe";
+var firstname = " john";
+var city = " new york"
+//alert("nom :" + lastname + "\nprénom :" + firstname + "\nville :" + city);
+
+//alert("nom :" + lastname + "\nprénom :" + firstname + "\nville :" + city);
+
+alert(`nom: ${lastname}\nprénom: ${firstname}\nville: ${city}`)
